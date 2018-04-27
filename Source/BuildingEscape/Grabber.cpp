964ad3866bfce91c5938555cd1e15600c54f94bc
aryyya <arya@live.ca>
2018-04-27 02:55:13 -0700

    Add modifier default pawn with grabber component.

---
+++ Source/BuildingEscape/Grabber.cpp
@@ -0,0 +1,18 @@
+#include "Grabber.h"
+
+UGrabber::UGrabber()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"))
+}
+
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
---
+++ Source/BuildingEscape/Grabber.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Grabber.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UGrabber : public UActorComponent
+{
+	GENERATED_BODY()
+	
+public:
+	UGrabber();
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	
+protected:
+	virtual void BeginPlay() override;
+};
