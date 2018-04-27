49d784361462189cfb247c44fb2bb4584ff1e8cc
aryyya <arya@live.ca>
2018-04-26 20:40:19 -0700

    Add door open trigger and link it to player.

--- Source/BuildingEscape/OpenDoor.cpp
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -1,40 +1,30 @@
-
-
 #include "OpenDoor.h"
 
-
-// Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
-
-// Called when the game starts
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-
-    // Find the owning Actor.
-    AActor* Owner = GetOwner();
     
-    // Create a rotator.
-    FRotator NewRotator = FRotator(0.0f, -60.0f, 0.0f);
-    
-    // Set the door rotation.
-    Owner->SetActorRotation(NewRotator);
+    ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
 }
 
-
-// Called every frame
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
+    
+    if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+    {
+        OpenDoor();
+    }
 }
 
+void UOpenDoor::OpenDoor()
+{
+    AActor* Owner = GetOwner();
+    FRotator NewRotator = FRotator(0.0f, -60.0f, 0.0f);
+    Owner->SetActorRotation(NewRotator);
+}
--- Source/BuildingEscape/OpenDoor.h
+++ Source/BuildingEscape/OpenDoor.h
@@ -1,29 +1,31 @@
-
-
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/TriggerVolume.h"
 #include "Components/ActorComponent.h"
 #include "OpenDoor.generated.h"
-
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
 {
 	GENERATED_BODY()
-
-public:	
-	// Sets default values for this component's properties
+    
+public:
 	UOpenDoor();
+    
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+private:
+    UPROPERTY(VisibleAnywhere)
+    float OpenAngle = 90.0f;
 
-		
-	
+    UPROPERTY(EditAnywhere)
+    ATriggerVolume* PressurePlate;
+    
+    AActor* ActorThatOpens;
+    
+    void OpenDoor();
 };
